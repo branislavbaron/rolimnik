cd4094b03c2e2f6292fa8170d49bd01f2b5f16f8
branislavbaron <branislavbaron@gmail.com>
2017-04-28 01:57:26 +0200

    Navbar section update

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,64 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
+var uglify = require('gulp-uglify');
 
-gulp.task('default', function() {
-  // place code for your default task here
+
+// Minify compiled CSS
+gulp.task('minify-css', ['sass'], function() {
+    return gulp.src('dist/css/*.css')
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
+
+// Minify JS
+gulp.task('minify-js', function() {
+    return gulp.src('dist/js/*.js')
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('dist/js'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+
+// Run everything
+gulp.task('default', ['sass', 'minify-css', 'minify-js']);
+
+
+// Dev task with browserSync
+gulp.task('dev', ['browserSync', 'minify-css', 'minify-js'], function() {
+    gulp.watch('dist/css/*.css', ['minify-css']);
+    gulp.watch('dist/js/*.js', ['minify-js']);
+    // Reloads the browser whenever HTML or JS files change
+    gulp.watch('*.html', browserSync.reload);
+    gulp.watch('dist/js/*.js', browserSync.reload);
+});
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("scss/*.scss", ['sass']);
+    gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("scss/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
