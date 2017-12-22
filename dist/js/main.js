7160db252b9dead698b9f42d16ffa36e493be05f
branislavbaron <branislavbaron@gmail.com>
2017-12-22 04:39:19 +0100

    add Logo and rework homepage

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,13 +1,34 @@
    $().ready(function(){
         $('[rel="tooltip"]').tooltip();
 
+        function rotateCard(btn){
+            var $card = $(btn).closest('.card');
+            if($card.hasClass('hover')){
+                $card.removeClass('hover');
+            } else {
+                $card.addClass('hover');
+            }
+        }
+
+// DOM CACHE
+        var $packages = $('.packages');
+        var $basicTraining = $('.basic-training');
+
+        $('.carousel').on('slid.bs.carousel', function () {
+            $basicTraining.removeClass('active');
+          var $drama = $('.carousel-indicators li.active').attr('data-slide-to');
+          var $finder = $('.basic-training[data-slide-to="' +$drama +'"]').addClass('active');
+        });
+
+        $('.btn-close-hrs').on('click touch', function(event) {
+            event.preventDefault();
+            $('.working-hours-menu').collapse('hide');
+        });
+
+        $packages.waypoint(function(){
+        $('.standard').addClass('animated tada');
+    }, {
+        offset: '30%'
     });
 
-    function rotateCard(btn){
-        var $card = $(btn).closest('.card');
-        if($card.hasClass('hover')){
-            $card.removeClass('hover');
-        } else {
-            $card.addClass('hover');
-        }
-    }
+    });
--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,100 @@ var cleanCSS = require('gulp-clean-css');
 var rename = require("gulp-rename");
 var uglify = require('gulp-uglify');
 
+var realFavicon = require ('gulp-real-favicon');
+var fs = require('fs');
+
+// File where the favicon markups are stored
+var FAVICON_DATA_FILE = 'faviconData.json';
+
+// Generate the icons. This task takes a few seconds to complete.
+// You should run it at least once to create the icons. Then,
+// you should run it whenever RealFaviconGenerator updates its
+// package (see the check-for-favicon-update task below).
+gulp.task('generate-favicon', function(done) {
+    realFavicon.generateFavicon({
+        masterPicture: 'img/rolimnikfavicon.png',
+        dest: '.',
+        iconsPath: '/',
+        design: {
+            ios: {
+                pictureAspect: 'noChange',
+                assets: {
+                    ios6AndPriorIcons: false,
+                    ios7AndLaterIcons: false,
+                    precomposedIcons: false,
+                    declareOnlyDefaultIcon: true
+                }
+            },
+            desktopBrowser: {},
+            windows: {
+                pictureAspect: 'noChange',
+                backgroundColor: '#da532c',
+                onConflict: 'override',
+                assets: {
+                    windows80Ie10Tile: false,
+                    windows10Ie11EdgeTiles: {
+                        small: false,
+                        medium: true,
+                        big: false,
+                        rectangle: false
+                    }
+                }
+            },
+            androidChrome: {
+                pictureAspect: 'noChange',
+                themeColor: '#ffffff',
+                manifest: {
+                    display: 'standalone',
+                    orientation: 'notSet',
+                    onConflict: 'override',
+                    declared: true
+                },
+                assets: {
+                    legacyIcon: false,
+                    lowResolutionIcons: false
+                }
+            },
+            safariPinnedTab: {
+                pictureAspect: 'silhouette',
+                themeColor: '#5bbad5'
+            }
+        },
+        settings: {
+            scalingAlgorithm: 'Mitchell',
+            errorOnImageTooSmall: false
+        },
+        markupFile: FAVICON_DATA_FILE
+    }, function() {
+        done();
+    });
+});
+
+// Inject the favicon markups in your HTML pages. You should run
+// this task whenever you modify a page. You can keep this task
+// as is or refactor your existing HTML pipeline.
+gulp.task('inject-favicon-markups', function() {
+    return gulp.src([ './*.html' ])
+        .pipe(realFavicon.injectFaviconMarkups(JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).favicon.html_code))
+        .pipe(gulp.dest('.'));
+});
+
+// Check for updates on RealFaviconGenerator (think: Apple has just
+// released a new Touch icon along with the latest version of iOS).
+// Run this task from time to time. Ideally, make it part of your
+// continuous integration system.
+gulp.task('check-for-favicon-update', function(done) {
+    var currentVersion = JSON.parse(fs.readFileSync(FAVICON_DATA_FILE)).version;
+    realFavicon.checkForUpdates(currentVersion, function(err) {
+        if (err) {
+            throw err;
+        }
+    });
+});
+
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src("dist/scss/rolinmik.scss")
+    return gulp.src("dist/scss/rolimnik.scss")
         .pipe(sass())
         .pipe(gulp.dest("dist/css"))
         .pipe(browserSync.stream());
@@ -15,7 +106,7 @@ gulp.task('sass', function() {
 
 // Minify compiled CSS
 gulp.task('minify-css', ['sass'], function() {
-    return gulp.src('dist/css/rolinmik.css')
+    return gulp.src('dist/css/rolimnik.css')
         .pipe(cleanCSS({debug: true}, function(details) {
             console.log(details.name + ': ' + details.stats.originalSize);
             console.log(details.name + ': ' + details.stats.minifiedSize);
@@ -29,7 +120,7 @@ gulp.task('minify-css', ['sass'], function() {
 
 // Minify JS
 gulp.task('minify-js', function() {
-    return gulp.src('dist/js/*.js')
+    return gulp.src('dist/js/main.js')
         .pipe(uglify())
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('dist/js'))
@@ -55,4 +146,4 @@ gulp.task('serve', ['sass', 'minify-css', 'minify-js'], function() {
 
 
 
-gulp.task('default', ['serve']);
+gulp.task('default', ['serve', 'check-for-favicon-update']);
